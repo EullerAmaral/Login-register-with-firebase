9fc41426ee99170d00e44f0042e86af0d5ed4dfd
Euller Amaral <eullergustavo63@gmail.com>
2023-07-10 00:19:48 -0300

    Dismiss Keyboard

--- LoginRegisterWithFirebase/Register/RegisterVC.swift
+++ LoginRegisterWithFirebase/Register/RegisterVC.swift
@@ -18,7 +18,19 @@ class RegisterVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.dismissKeyboard()
     }
 
+}
+
+extension RegisterVC {
+    func dismissKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(RegisterVC.dismissKeyboardTouchOutside))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissKeyboardTouchOutside() {
+        view.endEditing(true)
+    }
 }
